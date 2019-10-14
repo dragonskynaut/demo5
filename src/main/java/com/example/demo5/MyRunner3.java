4a18ae1b488707f62b34c1d8ff0a85cc26d2ec29
dragon <dragonskynaut@gmail.com>
2019-10-14 11:13:32 +0800

    h2 database: jdbc lombok

--- src/main/java/com/example/demo5/MyRunner3.java
+++ src/main/java/com/example/demo5/MyRunner3.java
@@ -1,5 +1,6 @@
 package com.example.demo5;
 
+import com.example.demo5.bean.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,8 @@ public class MyRunner3 implements CommandLineRunner {
     private static String col1="username";
     private static String col2="password";
     private static final String DML1 = "insert into users("+ col1 +","+col2+") values(?,?)";
+    private static final String DML2 ="select * from users";
+    private static final String DML3 ="select * from users where username = ?";
     private String[] namePairs = {"Mark pwd1", "dragon pwd2", "ken pwd3", "tim pwd4"};
     @Override
     public void run(String... args) throws Exception {
@@ -39,5 +42,11 @@ public class MyRunner3 implements CommandLineRunner {
         // template can batch update
         LOGGER.info("------batch update-------");
         jdbcTemplate.batchUpdate(DML1, splitNames);
+        LOGGER.info("---------query back for objects--------");
+        jdbcTemplate.query(DML2, (rs, rowNum) -> new User(rs.getString(col1), rs.getString(col2)))
+        .forEach(user -> LOGGER.info(user.toString()));
+        LOGGER.info("------- selective query --------");
+        jdbcTemplate.query(DML3, new Object[]{"Mark"}, (rs, rowNum) -> new User(rs.getString(col1), rs.getString(col2)))
+            .forEach(user -> LOGGER.info("***only query for mark:" + user.toString()));
     }
 }
